7bdf395be543b17cef1c2481823ad39e7bd709ad
Kyle Corry <kylecorry31@gmail.com>
2021-10-30 13:33:25 -0400

    Click ghost for traits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 export interface Ghost {
   name: string;
   evidence: Evidence[];
+  traits: string[];
 }
 
 export type Evidence =
@@ -34,82 +35,102 @@ export class AppComponent {
     {
       name: "Spirit",
       evidence: ["EMF Level 5", "Spirit Box", "Ghost Writing"],
+      traits: ["Can't hunt for 180 seconds after smudging (normally 90)"]
     },
     {
       name: "Wraith",
       evidence: ["EMF Level 5", "Spirit Box", "D.O.T.S Projector"],
+      traits: ["Can teleport near a player while not hunting", "Can't leave footprints", "Increased ghost activity when stepping in salt"]
     },
     {
       name: "Phantom",
       evidence: ["Spirit Box", "Fingerprints", "D.O.T.S Projector"],
+      traits: ["Taking a photo makes it disappear", "Sanity drops quickly while looking at the ghost", "Stays invisible longer during the hunt (only visible every 1 to 2 seconds)"]
     },
     {
       name: "Poltergeist",
       evidence: ["Spirit Box", "Fingerprints", "Ghost Writing"],
+      traits: ["Can throw multiple items at once", "Sanity decreases faster when multiple items are thrown"]
     },
     {
       name: "Banshee",
       evidence: ["Fingerprints", "Ghost Orb", "D.O.T.S Projector"],
+      traits: ["It will target a single player during hunts (ignores others)", "Navigates to target and will hunt if it sees it's target for long enough", "Crucifixes have a wider effective radius"]
     },
     {
       name: "Jinn",
       evidence: ["EMF Level 5", "Fingerprints", "Freezing Temperatures"],
+      traits: ["Runs faster while further from a player", "Can lower all players sanity by 25%", "Can't use abilities if the power is off"]
     },
     {
       name: "Mare",
       evidence: ["Spirit Box", "Ghost Orb", "Ghost Writing"],
+      traits: ["Increased hunt chance if in a dark room", "Can't turn on lights"]
     },
     {
       name: "Revenant",
       evidence: ["Ghost Orb", "Ghost Writing", "Freezing Temperatures"],
+      traits: ["Runs faster if it can see a player", "Runs slower if it can't see a player"]
     },
     {
       name: "Shade",
       evidence: ["EMF Level 5", "Ghost Writing", "Freezing Temperatures"],
+      traits: ["Less active when multiple players are in the room", "More likely to hunt when players are alone"]
     },
     {
       name: "Demon",
       evidence: ["Fingerprints", "Ghost Writing", "Freezing Temperatures"],
+      traits: ["Can hunt when sanity is high", "Successful ouija board answers take 40% sanity instead of 50%"]
     },
     {
       name: "Yurei",
       evidence: ["Ghost Orb", "Freezing Temperatures", "D.O.T.S Projector"],
+      traits: ["Sanity drops faster during ghost events or when near the ghost in a hunt", "Roams less when smudge sticks are used"]
     },
     {
       name: "Oni",
       evidence: ["EMF Level 5", "Freezing Temperatures", "D.O.T.S Projector"],
+      traits: ["More active when multiple players are in the room", "Can throw objects further"]
     },
     {
       name: "Hantu",
       evidence: ["Fingerprints", "Ghost Orb", "Freezing Temperatures"],
+      traits: ["Moves faster in cold rooms during hunts"]
     },
     {
       name: "Yokai",
       evidence: ["Spirit Box", "Ghost Orb", "D.O.T.S Projector"],
+      traits: ["Can hunt when sanity is high if players are talking", "Can only hear close voices during hunts"]
     },
     {
       name: "Goryo",
       evidence: ["EMF Level 5", "Fingerprints", "D.O.T.S Projector"],
+      traits: ["D.O.T.S only shows through video cameras", "Does not roam much"]
     },
     {
       name: "Myling",
       evidence: ["EMF Level 5", "Fingerprints", "Ghost Writing"],
+      traits: ["Can be detected by the parabolic mic more than other ghosts", "Footsteps can only be heard up close during a hunt (equal to flashlight flicker distance)"]
     },
     {
       name: "Onryo",
-      evidence: ["Spirit Box", "Ghost Orb", "Freezing Temperatures"]
+      evidence: ["Spirit Box", "Ghost Orb", "Freezing Temperatures"],
+      traits: ["Increased hunt chance when a flame is extinguished", "Increased hunt change after each player death"]
     },
     {
       name: "The Twins",
-      evidence: ["EMF Level 5", "Spirit Box", "Freezing Temperatures"]
+      evidence: ["EMF Level 5", "Spirit Box", "Freezing Temperatures"],
+      traits: ["Interactions around the map and in a specific room", "Multiple interactions at the same time"]
     },
     {
       name: "Raiju",
-      evidence: ["EMF Level 5", "Ghost Orb", "D.O.T.S Projector"]
+      evidence: ["EMF Level 5", "Ghost Orb", "D.O.T.S Projector"],
+      traits: ["Runs faster when near electronics", "Can hunt early when near active electronics"]
     },
     {
       name: "Obake",
-      evidence: ["EMF Level 5", "Fingerprints", "Ghost Orb"]
+      evidence: ["EMF Level 5", "Fingerprints", "Ghost Orb"],
+      traits: ["Can have a 6 finger fingerprint"]
     }
   ].sort((a, b) => a.name.localeCompare(b.name)) as Ghost[];
 
--- src/app/ghost/ghost.component.ts
+++ src/app/ghost/ghost.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { Ghost, Evidence } from '../app.component'
 
 @Component({
@@ -10,6 +10,11 @@ export class GhostComponent implements OnInit {
 
   @Input() ghost: Ghost;
   @Input() currentEvidence: Evidence[];
+
+  @HostListener("click")
+  onClick(){
+    alert(this.ghost.name + "\n\n" + this.ghost.traits.map(it => "- " + it).join("\n\n"));
+  }
 
   constructor() { }
 
