0734b709e68b05806a0a86670caabc9d279a46be
Kyle Corry <kylecorry31@gmail.com>
2021-11-29 15:29:36 -0500

    Delete angular build

--- src/app/models/evidence.ts
+++
@@ -1,9 +0,0 @@
-export enum Evidence {
-    FreezingTemperatures = "Freezing Temperatures",
-    Fingerprints = "Fingerprints",
-    SpiritBox = "Spirit Box",
-    EMFLevel5 = "EMF Level 5",
-    GhostWriting = "Ghost Writing",
-    GhostOrb = "Ghost Orb",
-    DOTSProjector = "D.O.T.S. Projector"
-}
--- src/app/models/ghost.ts
+++
@@ -1,8 +0,0 @@
-import { Evidence } from "./evidence";
-
-export interface Ghost {
-    id: number;
-    name: string;
-    evidence: Evidence[];
-    traits: string[];
-}
