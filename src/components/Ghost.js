63d1c6cbfa2fad71959ebb3a243926b274e68cd4
Kyle Corry <kylecorry31@gmail.com>
2021-11-30 06:46:44 -0500

    Fix ghost trait dialog

--- src/components/Ghost.js
+++ src/components/Ghost.js
@@ -4,7 +4,7 @@ import {FreezingTemperatures, Fingerprints, SpiritBox, EMFLevel5, GhostWriting, 
 
 const Ghost = ({ ghost, currentEvidence }) => {
   return (
-    <div className="ghost">
+    <div className="ghost" onClick={() => alert(ghost.name + "\n\n" + ghost.traits.map(it => "- " + it).join("\n\n"))}>
       <h4 className="ghost-name">{ghost.name}</h4>
       <div>
         <small>
